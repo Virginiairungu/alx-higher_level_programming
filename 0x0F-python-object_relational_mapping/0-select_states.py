f88c14f1009ebfbecbc2d08f7e6675b6886e680e
Virginiairungu <virginiairungu14@gmail.com>
2024-06-28 18:05:13 +0300

    Add SQL script and README for database setup

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,18 @@
-#!/usr/bin/python3
-"""  Your Comments Here """
-import MySQLdb
-import sys
+def new_func():
+    #!/usr/bin/python3
+    """  Your Comments Here """
+    import MySQLdb
+    import sys
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+    if __name__ == "__main__":
+        db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    c = db.cursor()
-    c.execute("SELECT * FROM `states`")
-    rows = c.fetchall()
-    for row in rows:
-        print(row)
-    c.close()
-    db.close()
+        c = db.cursor()
+        c.execute("SELECT * FROM `states`")
+        rows = c.fetchall()
+        for row in rows:
+            print(row)
+        c.close()
+        db.close()
+
+new_func()
--- 0x0F-python-object_relational_mapping/0-select_states.sql
+++ 0x0F-python-object_relational_mapping/0-select_states.sql
@@ -1,9 +1,17 @@
--- Create states table in hbtn_0e_0_usa with some data
-CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa;
-USE hbtn_0e_0_usa;
-CREATE TABLE IF NOT EXISTS states ( 
-    id INT NOT NULL AUTO_INCREMENT, 
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
+-- Drop `states` table if it exists and recreate it
+DROP TABLE IF EXISTS `states`;
+
+-- Create the `states` table
+CREATE TABLE `states` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(256) NOT NULL,
+    PRIMARY KEY (`id`)
 );
-INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
+
+-- Insert initial data into `states` table
+INSERT INTO `states` (`name`) VALUES
+    ('California'),
+    ('Arizona'),
+    ('Texas'),
+    ('New York'),
+    ('Nevada');
