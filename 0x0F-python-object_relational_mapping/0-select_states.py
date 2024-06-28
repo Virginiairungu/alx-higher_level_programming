9ec8fd2b884f2aedfc3dd268adb7cc92a510b665
Virginiairungu <virginiairungu14@gmail.com>
2024-06-28 15:06:29 +0300

    Fix issues in Python script and SQL script

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,33 +1,15 @@
-#!/usr/bin/env python3
-import sys
+#!/usr/bin/python3
+"""  Your Comments Here """
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
-    # Get MySQL credentials from command-line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-
-    # Connect to the MySQL server
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=db_name
-    )
-
-    # Create a cursor object to execute queries
-    cursor = db.cursor()
-
-    # Execute SQL query to select all states ordered by id
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
-    # Fetch all rows and print them
-    rows = cursor.fetchall()
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    rows = c.fetchall()
     for row in rows:
         print(row)
-
-    # Close the cursor and database connection
-    cursor.close()
+    c.close()
     db.close()
