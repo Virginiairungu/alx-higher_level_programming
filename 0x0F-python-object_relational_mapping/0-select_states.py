5331e7c629a813c91b4d16fde5a9cb559bafcd29
Virginiairungu <virginiairungu14@gmail.com>
2024-06-29 06:40:10 +0300

    Add SQL script and README for database setup

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,26 +1,18 @@
 #!/usr/bin/python3
-""" Lists all states from the database hbtn_0e_0_usa. """
+"""
+Lists all states from the database
+It takes 3 arguments: mysql username, mysql password and database name
+"""
+
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-    # Check for the correct number of arguments
-    if len(sys.argv) != 4:
-        print("Usage: ./0-select_states.py root C@m3R00n#2050# hbtn_0e_0_usa")
-        sys.exit(1)
-    
-    # Database connection
-    db = MySQLdb.connect(
-        host="localhost",
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        port=3306
-    )
-    c = db.cursor()
-    c.execute("SELECT * FROM `states` ORDER BY `id` ASC")
-    rows = c.fetchall()
-    for row in rows:
-        print(row)
-    c.close()
+    db = MySQLdb.connect(host="localhost",
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+    for state in cursor.fetchall():
+        print(state)
+    cursor.close()
     db.close()
