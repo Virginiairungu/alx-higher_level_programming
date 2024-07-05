fcb99e35c8a4e299f062d0c2d2e9b6c7352a499d
Virginiairungu <virginiairungu14@gmail.com>
2024-07-04 17:51:32 -0700

    6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,25 +1,14 @@
 #!/usr/bin/python3
-"""Finds a peak in a list of unsorted integers"""
+""" Finds peak in list of unsorted integers"""
 
 
 def find_peak(list_of_integers):
-    """Finds a peak in a list of unsorted integers"""
-    if not list_of_integers:
+    """Find highest value in list of unsorted integers"""
+
+    my_list = list_of_integers
+
+    if my_list:
+        my_list.sort()
+        return my_list[-1]
+    else:
         return None
-
-    def binary_search_peak(low, high):
-        mid = (low + high) // 2
-        
-        # Check if mid is a peak
-        if (mid == 0 or list_of_integers[mid] >= list_of_integers[mid - 1]) and \
-           (mid == len(list_of_integers) - 1 or list_of_integers[mid] >= list_of_integers[mid + 1]):
-            return list_of_integers[mid]
-        # If left neighbor is greater, the peak must be on the left side
-        elif mid > 0 and list_of_integers[mid - 1] > list_of_integers[mid]:
-            return binary_search_peak(low, mid - 1)
-        # If right neighbor is greater, the peak must be on the right side
-        else:
-            return binary_search_peak(mid + 1, high)
-
-    return binary_search_peak(0, len(list_of_integers) - 1)
-
