e57a39079796e95891e9cad77c0f865b9892ca8b
Virginiairungu <virginiairungu14@gmail.com>
2024-07-04 17:38:27 -0700

    6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python3
-""" Finds peak in list of unsorted integers"""
-
+"""
+A function that finds a peak in a list of unsorted integers.
+"""
 
 def find_peak(list_of_integers):
-    """Find highest value in list of unsorted integers"""
-
-    my_list = list_of_integers
-
-    if my_list:
-        my_list.sort()
-        return my_list[-1]
-    else:
+    """
+    Return a peak in a list of unsorted integers
+    """
+    if not list_of_integers:
         return None
+
+    def binary_search_peak(low, high):
+        mid = (low + high) // 2
+        if (mid == 0 or list_of_integers[mid] >= list_of_integers[mid - 1]) and \
+           (mid == len(list_of_integers) - 1 or list_of_integers[mid] >= list_of_integers[mid + 1]):
+            return list_of_integers[mid]
+        elif mid > 0 and list_of_integers[mid - 1] > list_of_integers[mid]:
+            return binary_search_peak(low, mid - 1)
+        else:
+            return binary_search_peak(mid + 1, high)
+
+    return binary_search_peak(0, len(list_of_integers) - 1)
+
